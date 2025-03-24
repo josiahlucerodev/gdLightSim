9174cceacba7200b5f16af8b9ce76c122f335943
Liam Yeager <lcyeager@ncsu.edu>
2025-03-24 12:18:47 -0400

    [impl] added rotation button to mirrors

--- project/demo/node_2d.gd
+++ project/demo/node_2d.gd
@@ -1,4 +1,12 @@
 extends Node2D
-
+var rotating = false
+var ang_velocity = 0
 func _process(delta: float) -> void:
-	transform = transform.rotated(0.01)
+	if Input.is_action_pressed("cw"):
+		ang_velocity = 0.01
+	if Input.is_action_pressed("ccw"):
+		ang_velocity = -0.01
+	if !Input.is_action_pressed("ccw") && !Input.is_action_pressed("cw"):
+		ang_velocity = 0
+	global_transform = global_transform.rotated(ang_velocity)
+	
--- project/demo/player.gd
+++ project/demo/player.gd
@@ -12,3 +12,6 @@ func _physics_process(delta):
 		velocity.y = move_toward(velocity.y, 0, SPEED)
 		
 	move_and_slide()
+
+
+	
